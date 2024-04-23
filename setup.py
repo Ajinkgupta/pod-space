9bd7d0072b13d50d7681df02282a46a524e41b39
Ajink Gupta <guptaajink72@gmail.com>
2024-04-23 12:41:17 +0000

    l

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='daac',
     version='0.1',
-    packages=['daac'],
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A package to echo contents of programs.py to clipboard',
+    packages=find_packages(),
+    install_requires=[
+        'pyperclip'
+    ],
     entry_points={
         'console_scripts': [
             'daac = daac.programs:main'
         ]
-    }
+    },
+    # Add this line to create a .whl file
+    setup_requires=['wheel'],
+    # Add these lines to create a .whl file
+    python_requires='>=3.6',
+    zip_safe=False,
 )
